d36cb73850df76fb7448f0831c0d4ae224cfcd72
jasl <jasl9187@hotmail.com>
2012-12-14 15:11:50 +0800

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,21 +16,4 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
 
-  def render_404
-    render_optional_error_file(404)
-  end
-
-  def render_403
-    render_optional_error_file(403)
-  end
-
-  def render_optional_error_file(status_code)
-    status = status_code.to_s
-    if %w(404 403 422 500).include?(status)
-      render :template => "/errors/#{status}", :format => [:html], :handler => [:erb], :status => status, :layout => "application"
-    else
-      render :template => "/errors/500", :format => [:html], :handler => [:erb], :status => status, :layout => "application"
-    end
-  end
-
 end
